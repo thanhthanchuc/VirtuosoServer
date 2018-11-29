ac61f1458936ae0b62e1f37d5eba434c9b2170d3
toiye <toiye@DESKTOP-BJB9GPG>
2018-11-29 14:17:17 +0700

    Get file contents and list of statement inside

---
+++ src/main/java/file/list/contents/getFileContents.java
@@ -0,0 +1,35 @@
+package file.list.contents;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class getFileContents {
+	//Get List of member
+	public static ArrayList<String> getListFiles(String fileName) throws FileNotFoundException {
+		ArrayList<String> list = new ArrayList<String>();
+		File file = new File(fileName);
+		try (Scanner inputStream = new Scanner(file)) {
+			while (inputStream.hasNext()) {
+				String data = inputStream.nextLine();
+				//Truong hop data co dang chua ki tu ". VD: "HaNoi"
+				if(data.length() != 2 && data.endsWith("\"")) {
+					data = data.substring(1, data.length() - 1);
+					list.add(data);
+					continue;
+				}
+				if(data.length() != 0 && !data.contains(",")) {
+					list.add(data);
+					continue;
+				}
+			}
+		}
+		if(list.size() == 1) {
+			String s = list.get(0);
+			return new ArrayList<String>(Arrays.asList(s.split(",")));
+		}
+		return list;
+	}
+}
---
+++ src/main/java/file/list/contents/getPathFile.java
@@ -0,0 +1,12 @@
+package file.list.contents;
+
+public class getPathFile {
+	//Change here
+	protected static final String pathCity = "src/main/java/file/ListCity.txt";
+	protected static final String pathCountry = "src/main/java/file/ListCountry.txt";
+	protected static final String pathFirstName = "src/main/java/file/ListFirstName.txt";
+	protected static final String pathLastName = "src/main/java/file/ListLastName.txt";
+	// protected static final String pathVerbForIRI =
+	// "src/main/java/file/list_contents/ListVerbForIRI.txt";
+	protected static final String pathPosition = "src/main/java/file/ListPosition.txt";
+}
