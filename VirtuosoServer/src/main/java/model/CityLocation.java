2a4d917c792cad510325ec838d058703cd3f6ad6
Le Dinh Phuc <ledinhphucts98@gmail.com>
2018-12-18 21:16:41 +0700

    Done !

--- VirtuosoServer/src/main/java/model/CityLocation.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class CityLocation extends EntityModel {
-	public CityLocation(String name, String detail, String link, String timeLink) {
-		super(name, detail, link + " _ (" + timeLink + ").");
-	}
-}
--- VirtuosoServer/src/main/java/model/Country.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Country extends EntityModel {
-	public Country(String name, String detail, String link, String timeLink) {
-		super(name, detail, link + " _ (" + timeLink + ").");
-	}
-}
